13974cf34648ed64ca7b89cb996b0dad23fef79f
David Fernando M <david.mesa1@misena.edu.co>
2023-05-04 16:30:27 -0500

    fix: Correccion a el modulo de biblioteca

--- src/biblioteca/biblioteca.module.ts
+++ src/biblioteca/biblioteca.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { BibliotecaController } from './controllers/biblioteca.controller';
+import { ReservaLibrosController } from './controllers/reserva-libros.controller';
+import { BibliotecaService } from './services/biblioteca.service';
+import { ReservaLibroService } from './services/reserva-libro.service';
+
+@Module({
+  controllers: [BibliotecaController, ReservaLibrosController],
+  providers: [BibliotecaService, ReservaLibroService],
+})
 export class BibliotecaModule {}
